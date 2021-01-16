63f03bcdb5d83e774c5ef6965d646aba15476826
Vitalii Shytikov <v.shytikov@gmail.com>
2021-01-16 13:30:05 +0200

    Add stories for input

--- src/components/Input/index.stories.js
+++ src/components/Input/index.stories.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Input, ChatbotPhoneInput } from './index';
+import {
+  Input,
+  ChatbotPhoneInput,
+  InputWithIcon,
+  DraggableInput,
+  ColorInput,
+} from './index';
 import { PrimaryButton } from '../Button';
 
 export default {
@@ -16,12 +22,15 @@ export default {
 
 const Default = (args) => <Input {...args} />;
 const ChatbotPhone = (args) => <ChatbotPhoneInput {...args} />;
+const IconInput = (args) => <InputWithIcon {...args} />;
+const ColorIcon = (args) => <ColorInput {...args} />;
+const DragIcon = (args) => <DraggableInput {...args} />;
 
 export const Standard = Default.bind({});
 Standard.args = {};
 
-export const StandardWithError = Default.bind({});
-StandardWithError.args = {
+export const WithError = Default.bind({});
+WithError.args = {
   error: "can't be less than 1",
 };
 
@@ -29,15 +38,26 @@ export const ChatbotWithActionButton = ChatbotPhone.bind({});
 ChatbotWithActionButton.args = {
   disabled: true,
   value: '+380123456789',
-  actionButton: (
-    <PrimaryButton >
-      Copy
-    </PrimaryButton>
-  ),
+  actionButton: <PrimaryButton>Copy</PrimaryButton>,
 };
 
 export const ChatbotWhenLoading = ChatbotPhone.bind({});
 ChatbotWhenLoading.args = {
   disabled: true,
   isLoading: true,
+};
+
+export const WithIcon = IconInput.bind({});
+WithIcon.args = {
+  type: 'number',
+  icon: '$',
+};
+
+export const Draggable = DragIcon.bind({});
+Draggable.args = { };
+
+export const Colorful = ColorIcon.bind({});
+Colorful.args = {
+  color: '#f05454',
+  value: '#f05454',
 };
