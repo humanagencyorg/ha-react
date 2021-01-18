f4a4b596165d7fb9c4579b78314ca107e120e7d8
Vitalii Shytikov <v.shytikov@gmail.com>
2021-01-18 15:21:38 +0200

    Export Button and Input components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 export { MentionsInput } from './components/MentionsInput';
+export { Input, ChatbotPhoneInput, InputWithIcon, DraggableInput, ColorInput } from './components/Input';
+export { Button, PrimaryButton, SubmissionButton, NewItemButton, SmallDeleteButton, LinkButton, PrimaryLinkButton, ChatbotLoadingButton } from './components/Button';
