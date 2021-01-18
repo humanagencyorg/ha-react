a47953222a055d0c2a64204de5367f83f1265646
Vitalii Shytikov <v.shytikov@gmail.com>
2021-01-18 20:40:10 +0200

    Export InlineEditInput

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { MentionsInput } from './components/MentionsInput';
 export { Input, ChatbotPhoneInput, InputWithIcon, DraggableInput, ColorInput } from './components/Input';
+export { InlineEditInput} from './components/Input/InlineEditInput';
 export { Button, PrimaryButton, SubmissionButton, NewItemButton, SmallDeleteButton, LinkButton, PrimaryLinkButton, ChatbotLoadingButton } from './components/Button';
