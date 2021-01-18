e2fff6896787db46e7ef9be7a9e2c3768f3c7929
Vitalii Shytikov <v.shytikov@gmail.com>
2021-01-18 17:51:41 +0200

    Revert "Export Button and Input components"
    This reverts commit f4a4b596165d7fb9c4579b78314ca107e120e7d8.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
 export { MentionsInput } from './components/MentionsInput';
-export { Input, ChatbotPhoneInput, InputWithIcon, DraggableInput, ColorInput } from './components/Input';
-export { Button, PrimaryButton, SubmissionButton, NewItemButton, SmallDeleteButton, LinkButton, PrimaryLinkButton, ChatbotLoadingButton } from './components/Button';
