63f60942f4c1ba9be74d8f475664dca6fec99b4c
Vitalii Shytikov <v.shytikov@gmail.com>
2021-01-19 15:31:49 +0200

    Revert "Export InlineEditInput"
    This reverts commit a47953222a055d0c2a64204de5367f83f1265646.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 export { MentionsInput } from './components/MentionsInput';
 export { Input, ChatbotPhoneInput, InputWithIcon, DraggableInput, ColorInput } from './components/Input';
-export { InlineEditInput} from './components/Input/InlineEditInput';
 export { Button, PrimaryButton, SubmissionButton, NewItemButton, SmallDeleteButton, LinkButton, PrimaryLinkButton, ChatbotLoadingButton } from './components/Button';
